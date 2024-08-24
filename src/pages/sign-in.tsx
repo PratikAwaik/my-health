b5e0b18ca1538af73a4c2707160fca6ec8f2ab40
PratikAwaik <pratikawaik125@gmail.com>
2024-08-24 22:29:23 +0530

    add shadcn button and sign in with epic button

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function SignIn() {
-  return <div className="w-full h-full flex items-center justify-center"></div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Button>Sign In with EPIC</Button>
+    </div>
+  );
 }
