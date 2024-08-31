6b5d60106255a3037a041cbcaa7ff135c39f1bf9
PratikAwaik <pratikawaik125@gmail.com>
2024-09-01 04:10:50 +0530

    add basic info in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,43 +1,82 @@
-import { Clock } from "lucide-react";
+import { Clock, ExternalLink } from "lucide-react";
 import { useMemo } from "react";
-import { Card, CardHeader, CardTitle } from "./ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Link } from "react-router-dom";
+import { Patient } from "fhir/r4";
+import { Badge } from "./ui/badge";
+import { format } from "date-fns";
+import { getCookie } from "@/utils/cookies";
+import { COOKIE_KEYS } from "@/utils/constants";
+import { Button } from "./ui/button";
 
 interface NavbarProps {
-  patientName?: string;
+  patient: Patient;
 }
 
-export function Navbar({ patientName }: NavbarProps) {
-  const nameInitials = useMemo(() => {
-    if (patientName) {
-      const split = patientName.split(" ");
-      let initials = "";
-      const [firstName, middleName, lastName] = split;
-      if (firstName) initials += firstName.substring(0, 1);
-      if (lastName) initials += lastName.substring(0, 1);
-      else if (middleName) initials += middleName.substring(0, 1);
-      return initials;
+export function Navbar({ patient }: NavbarProps) {
+  const primaryIdentifier = useMemo(
+    () => getCookie(COOKIE_KEYS.PATIENT_ID),
+    []
+  );
+
+  const patientName = useMemo(() => {
+    if (patient) {
+      const names = patient.name;
+
+      const officialName = names?.find((name) => name.use === "official");
+      const usualName = names?.find((name) => name.use === "usual");
+      if (officialName) {
+        return officialName.text;
+      } else if (usualName) {
+        return usualName.text;
+      }
     }
-  }, [patientName]);
+  }, [patient]);
+
+  // const nameInitials = useMemo(() => {
+  //   if (patientName) {
+  //     const split = patientName.split(" ");
+  //     let initials = "";
+  //     const [firstName, middleName, lastName] = split;
+  //     if (firstName) initials += firstName.substring(0, 1);
+  //     if (lastName) initials += lastName.substring(0, 1);
+  //     else if (middleName) initials += middleName.substring(0, 1);
+  //     return initials;
+  //   }
+  // }, [patientName]);
 
   return (
     <Card>
       <CardHeader className="w-full flex flex-row items-center justify-between gap-x-4 flex-wrap px-4 py-3 space-y-0">
-        <CardTitle>Welcome, {patientName?.split(" ")[0]}</CardTitle>
+        <CardTitle>Welcome, {patientName}</CardTitle>
         <div className="flex items-center gap-x-4 mt-0">
           <div className="flex items-center gap-x-1 text-gray-500 text-sm">
             <Clock className="size-4" />
             <p>Session expires in: 0:00</p>
           </div>
-          <Link to={"/profile"}>
-            <Avatar>
-              <AvatarImage />
-              <AvatarFallback>{nameInitials}</AvatarFallback>
-            </Avatar>
-          </Link>
         </div>
       </CardHeader>
+      <CardContent>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-x-2">
+            <Badge className="text-sm">ID: {primaryIdentifier}</Badge>
+            <Badge variant="secondary" className="text-sm capitalize">
+              {patient.gender}
+            </Badge>
+            {!!patient.birthDate && (
+              <Badge variant="secondary" className="text-sm">
+                {format(new Date(patient.birthDate), "do MMM yyyy")}
+              </Badge>
+            )}
+          </div>
+          <Link to="/profile">
+            <Button variant="link">
+              <ExternalLink className="size-4" />
+              View full profile
+            </Button>
+          </Link>
+        </div>
+      </CardContent>
     </Card>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,18 @@
 import { Navbar } from "@/components/navbar";
+import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
+import { useAuth } from "@/hooks/use-auth";
 import { useGetPatientDetails } from "@/services/patient/patient.data";
 import { COOKIE_KEYS } from "@/utils/constants";
 import { getCookie } from "@/utils/cookies";
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
+  const { handleLogout } = useAuth();
 
   const { patient, isLoading: isPatientLoading } = useGetPatientDetails();
-
-  const patientName = useMemo(() => {
-    if (patient) {
-      const names = patient.name;
-
-      const officialName = names?.find((name) => name.use === "official");
-      const usualName = names?.find((name) => name.use === "usual");
-      if (officialName) {
-        return officialName.text;
-      } else if (usualName) {
-        return usualName.text;
-      }
-    }
-  }, [patient]);
-
-  console.log(patient);
 
   useEffect(() => {
     const access_token = getCookie(COOKIE_KEYS.ACCESS_TOKEN);
@@ -42,9 +29,22 @@ export default function Home() {
     );
   }
 
+  if (!patient) {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        <div className="flex flex-col gap-y-4 items-center">
+          <p className="text-3xl font-semibold">Patient data not found!</p>
+          <Button onClick={handleLogout} className="w-fit">
+            Sign In Again
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
-      <Navbar patientName={patientName} />
+      <Navbar patient={patient} />
       <div className="grid grid-cols-[25rem_2fr] gap-4 mt-4">
         <div></div>
         <div></div>
